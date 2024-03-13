13a989176ec9a8f07302240f3998dfae6719fd1b
rohit-iwnl <rohitmanivel9@gmail.com>
2024-03-12 20:13:52 -0700

    Add tab bars for navigation

--- Wallpaper Engine/Controllers/UpcomingViewController.swift
+++ Wallpaper Engine/Controllers/UpcomingViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  UpcomingViewController.swift
 //  Wallpaper Engine
 //
 //  Created by Rohit Manivel on 3/12/24.
@@ -7,13 +7,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class UpcomingViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .systemBackground
     }
 
-
 }
-
---
+++ Wallpaper Engine/MainTabBarViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  Wallpaper Engine
+//
+//  Created by Rohit Manivel on 3/12/24.
+//
+
+import UIKit
+
+class MainTabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemYellow
+        
+        let vc1 = UINavigationController(rootViewController: HomeViewController())
+        let vc2 = UINavigationController(rootViewController: UpcomingViewController())
+        let vc3 = UINavigationController(rootViewController: SearchViewController())
+        let vc4 = UINavigationController(rootViewController: DownloadViewController())
+        
+        vc1.tabBarItem.image = UIImage(systemName: "house")
+        vc2.tabBarItem.image = UIImage(systemName: "play.circle")
+        vc3.tabBarItem.image = UIImage(systemName: "magnifyingglass")
+        vc4.tabBarItem.image = UIImage(systemName: "arrow.down.to.line")
+        
+        vc1.title = "Home"
+        vc2.title = "Upcoming"
+        vc3.title = "Search"
+        vc4.title = "Downloads"
+        
+        tabBar.tintColor = .label
+        
+        setViewControllers([vc1,vc2,vc3,vc4], animated: true)
+    }
+
+
+}
+
